774880db5e2c68adba00c2b3f87e3e579671ba12
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2017-09-10 18:17:37 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './stackerine-logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Get a career as a web developer</h2>
+        </div>
+        <p className="App-intro">
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
